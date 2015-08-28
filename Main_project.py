f328a2ce89ffcad5cc4014211a7e94c9ae5154b2
Mike <eemos@leeds.ac.uk>
2015-08-28 11:57:42 +0100

    Working version

--- Main_project.py
+++ Main_project.py
@@ -1,10 +1,21 @@
-slp_months = np.empty([36,241,480)]
-slp_all    = np.empty([12,36,241,480)]
 
-for j in (0,12):
-    for i in (0, 36):
+from netCDF4 import Dataset
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+Data=Dataset('netcdf-atls03-a562cefde8a29a7288fa0b8b7f9413f7-LtAc4K.nc')
+
+slp=Data.variables['msl']
+
+slp_months = np.empty([36,241,480])
+slp_all    = np.empty([12,36,241,480])
+
+for j in range(0,12):
+    for i in range(0, 36):
         a  = (i * 12) + j
-        slp_months[i,:,:] = slp.Dataset[,:,:]
+        slp_months[i,:,:] = slp[a,:,:]
     slp_all[j,:,:,:] = slp_months
 
 
